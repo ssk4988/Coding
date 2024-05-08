ad6e10e3db1eb0baed5a88c6f1e22641f8fcf737
Sachin S <ssk4988@gmail.com>
2024-05-08 09:27:15 -0400

    modify template and gitignore

--- name.cpp
+++ name.cpp
@@ -3,27 +3,21 @@ using namespace std;
 
 using ll = long long;
 using ld = long double;
-using pi = pair<int, int>;
-using pl = pair<ll, ll>;
-using pd = pair<ld, ld>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
 using vi = vector<int>;
 using vl = vector<ll>;
 using vd = vector<ld>;
-using vpi = vector<pi>;
-using vpl = vector<pl>;
-using vpd = vector<pd>;
+using vii = vector<pii>;
+using vll = vector<pll>;;
 using vvi = vector<vi>;
 
 #define f first
 #define s second
-#define mp make_pair
 #define pb push_back
-#define lb lower_bound
-#define ub upper_bound
 #define all(x) x.begin(), x.end()
 #define sz(x) (int)(x).size()
 #define rep(i, a, b) for (int i = a; i < (b); ++i)
-#define nL "\n"
 
 int main()
 {
