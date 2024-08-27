1403ff38609fd7e7f985543f4c9eb018ffb918b8
Sachin S <ssk4988@gmail.com>
2024-08-27 14:48:30 -0400

    attempt Programmer's Will

--- UCFPT/19locals/j.cpp
+++ UCFPT/19locals/j.cpp
@@ -1,3 +1,4 @@
+#pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -9,75 +10,126 @@ using pii = pair<int, int>;
 using vii = vector<pii>;
 using vvi = vector<vi>;
 
-#define rep(i, a, b) for(int i = a; i < (b); i++)
+#define rep(i, a, b) for (int i = a; i < (b); i++)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)x.size()
 
-const int N = 1000;
-using mat = array<array<ld, N>, N>;
+int N;
+// using mat = array<array<ld, N>, N>;
+using mat = vector<ld>;
 
-mat operator*(mat &a, mat &b) {
-    mat res{};
-    rep(i, 0, N){
-        rep(j, 0, N){
-            rep(k, 0, N){
-                // if(k == i && j != i) continue;
-                res[i][j] += a[i][k] * a[k][j];
-            }
-        }
-    }
-    return res;
+vi use;
+
+inline int idx(int i, int j)
+{
+    return i * N + j;
 }
 
-int main() {
+int main()
+{
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
-    int n, m; cin >> n >> m;
-    mat base{};
+    int n, m;
+    cin >> n >> m;
+    N = 2 * n;
+    mat base(N * N);
     vector<vector<pair<int, ld>>> adj(n);
     vi lolly(n);
-    rep(i, 0, n){
+    vi out(n);
+    rep(i, 0, n)
+    {
         cin >> lolly[i];
-        int k; cin >> k;
-        rep(j, 0, k){
-            int to; ld w; cin >> to >> w; to--;
-            if(to < n){
-                base[i][to] += w;
-            } else {
+        int k;
+        cin >> k;
+        rep(j, 0, k)
+        {
+            int to;
+            ld w;
+            cin >> to >> w;
+            to--;
+            if (to < n)
+            {
+                base[idx(i, to)] += w;
+            }
+            else
+            {
                 adj[i].push_back({to, w});
-                base[i][N/2 + i] += w;
+                base[idx(i, N / 2 + i)] += w;
+                out[i] = true;
             }
         }
     }
-    rep(i, N/2, N) base[i][i] = 1;
-    rep(i, 0, 5){
-        base = base * base;
+    rep(i, 0, n)
+    {
+        rep(j, 0, n)
+        {
+            rep(k, 0, n)
+            {
+                if (out[k])
+                    out[j] = 1;
+            }
+        }
+    }
+    rep(i, 0, n) if (out[i]) use.push_back(i);
+    rep(i, N / 2, N) base[idx(i, i)] = 1;
+    rep(i, 0, 25)
+    {
+        mat res(N * N);
+        for (int i : use)
+        {
+            rep(j, 0, N)
+            {
+                for (int k : use)
+                {
+                    // rep(k, 0, N/2){
+                    // if(k == i && j != i) continue;
+                    res[idx(i, j)] += base[idx(i, k)] * base[idx(k, j)];
+                    // res[i][j] += a[i][k] * b[k][j];
+                }
+            }
+            rep(j, N / 2, N)
+            {
+                res[idx(i, j)] += base[idx(i, j)] * base[idx(j, j)];
+                // res[i][j] += a[i][j] * b[j][j];
+            }
+        }
+        rep(i, N / 2, N)
+        {
+            res[idx(i, i)] += base[idx(i, i)] * base[idx(i, i)];
+            // res[i][i] += a[i][i] * b[i][i];
+        }
+        swap(base, res);
     }
     vector<ld> res(m);
     vector<ld> stay(m);
-    rep(i, 0, n){
-        rep(j, 0, n){
-            stay[j] += lolly[i] * base[i][j+N/2];
+    rep(i, 0, n)
+    {
+        rep(j, 0, n)
+        {
+            stay[j] += lolly[i] * base[idx(i, j + N / 2)];
         }
     }
-    rep(i, 0, n){
-        if(sz(adj[i]) == 0) continue;
+    rep(i, 0, n)
+    {
+        if (sz(adj[i]) == 0)
+            continue;
         ld sum = 0;
-        for(auto [to, w] : adj[i]){
+        for (auto [to, w] : adj[i])
+        {
             sum += w;
         }
-        for(auto [to, w] : adj[i]){
+        for (auto [to, w] : adj[i])
+        {
             stay[to] += w / sum * stay[i];
         }
     }
     cout << fixed << setprecision(8);
-    // rep(i, 0, n){
-    //     cout << stay[i] << "\n";
-    // }
-    // cout << "actual: \n";
-    rep(i, 0, m){
-        if(i < n) cout << 0 << "\n";
-        else cout << stay[i] << "\n";
+    rep(i, 0, m)
+    {
+        if (i < n)
+            cout << 0 << "\n";
+        else
+            cout << stay[i] << "\n";
     }
 
     return 0;
