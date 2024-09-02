fe888d2dfa9a39827830ec6ff203098087052beb
Sachin S <ssk4988@gmail.com>
2024-09-02 17:48:37 -0400

    edit please to be shorter

--- UCFPT/24locals/final/please.cpp
+++ UCFPT/24locals/final/please.cpp
@@ -25,47 +25,39 @@ int main()
     cin.exceptions(cin.failbit);
     int n; cin >> n;
     vi grade(n), letter(n), ps(n);
-    vi priorset;
     rep(i, 0, n){
         cin >> grade[i];
         string l; cin >> l;
         letter[i] = l[0] - 'A';
         if(l[0] == 'F') letter[i] = 4;
         cin >> ps[i];
-        priorset.pb(ps[i]);
     }
-    sort(all(priorset));
-    reverse(all(priorset));
-    map<int, int> priormp;
-    for(int p : priorset){
-        if(!priormp.count(p)) priormp[p] = sz(priormp);
-    }
-    vector<vvi> students(5, vvi(101));
+    vi plist(ps);
+    sort(all(plist));
+    plist.erase(unique(all(plist)), plist.end());
+    reverse(all(plist));
+    vector<vvi> students(5, vvi(101)), dp(5, vvi(101));
     rep(i, 0, n) {
-        ps[i] = priormp[ps[i]];
+        ps[i] = find(all(plist), ps[i]) - plist.begin();
         students[letter[i]][grade[i]].pb(ps[i]);
     }
-    vector dp(5, vvi(101));
     // dp[i][j] = {scores, A, B, C, D};
     // i = grade, j = grade boundary
     // dp[i][j] = add students[i][j] + max(dp[i][j-1], dp[i+1][j-1])
     auto go = [&](int l, int g, auto &&go) -> vi {
         if(sz(dp[l][g])) return dp[l][g];
-        dp[l][g] = vi(sz(priormp)+4);
+        dp[l][g] = vi(sz(plist)+4);
         for(int i : students[l][g]) dp[l][g][i]++;
-        // if(l == 4) return dp[l][g];
-        // if(g == 0) {
-        //     assert(false);
-        // }
-        if(l < 4) dp[l][g][sz(priormp)+l] = g;
+        if(l < 4) dp[l][g][sz(plist)+l] = g;
         if(l < 4 && g > 0){
             // next letter grade
             vi cur = go(l+1, g-1, go);
             for(int i : students[l][g]) cur[i]++;
-            cur[sz(priormp)+l] = g;
+            cur[sz(plist)+l] = g;
             dp[l][g] = max(dp[l][g], cur);
         }
         if(l + g > 4) {
+            // move boundary down
             vi cur = go(l, g-1, go);
             for(int i : students[l][g]) cur[i]++;
             dp[l][g] = max(dp[l][g], cur);
@@ -73,13 +65,9 @@ int main()
         return dp[l][g];
     };
     auto res = go(0, 100, go);
-    rep(i, sz(priormp), sz(res)){
-        cout << res[i];
-        if(i + 1 < sz(res)) cout << " ";
+    rep(i, sz(plist), sz(res)){
+        cout << res[i] << " \n"[i+1 == sz(res)];
     }
-    cout << "\n";
-
-
     
     return 0;
 }
